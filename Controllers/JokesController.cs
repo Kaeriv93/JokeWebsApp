5dcd29f67e9c64fdffff0f69615e4848d5ed07a1
Vireak San <vsan93@yahoo.com>
2022-09-21 23:52:40 -0500

    search query is now displaying correctly. Used an anonymous arrow function in our jokes controller to render out the view

--- Controllers/JokesController.cs
+++ Controllers/JokesController.cs
@@ -31,6 +31,13 @@ namespace JokeApp.Controllers
             return View();
         }
 
+        //POST: Jokes/ShowSearchResults
+        public async Task<IActionResult> ShowSearchResults(String SearchPhrase)
+        {
+            return View("Index", await _context.Joke.Where( j => j.JokeQuestion.Contains(SearchPhrase) ).ToListAsync());
+
+        }
+
         // GET: Jokes/Details/5
         public async Task<IActionResult> Details(int? id)
         {
